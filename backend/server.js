c5f32aa11abb16ad3a5971fdd71ededfaaa14694
itz-Hiru <hirumithagithub@gmail.com>
2025-05-20 13:35:41 +0530

    Created basic server

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Middleware
+app.use(express.json());
+
+// Routes
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
