4dd15e3db5918b388f9e56ac61244a0013c90127
itz-Hiru <hirumithagithub@gmail.com>
2025-05-28 18:03:40 +0530

    Created UI for Q&A

--- frontend/src/pages/Interview/Interview.page.jsx
+++ frontend/src/pages/Interview/Interview.page.jsx
@@ -1,9 +1,113 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import moment from "moment";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import DashboardLayout from "../../components/Layouts/DashboardLayout.component";
+import RoleInfoHeader from "../../components/Navbar/RoleInfoHeader.component";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPath";
+import QuestionCard from "../../components/Cards/QuestionCard.component";
 
 const Interview = () => {
-  return (
-    <div>Interview</div>
-  )
-}
+  const { sessionId } = useParams();
 
-export default Interview
+  const [sessionData, setSessionData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [openLearnMoreDrawer, setOpenLearnMoreDrawer] = useState(false);
+  const [explanation, setExplanation] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isUpdateLoader, setIsUpdateLoader] = useState(false);
+
+  // Fetch session data by session ID
+  const fetchSessionDataById = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.SESSION.GET_ONE(sessionId)
+      );
+
+      if (response.data && response.data.session) {
+        setSessionData(response.data.session);
+      }
+    } catch (error) {
+      console.error("Error while fetching: ", error);
+    }
+  };
+
+  // Generate concept explanations
+  const generateConceptExplanation = async (question) => {};
+
+  // Pin/Unpin Question
+  const togglePinQuestion = async (questionId) => {};
+
+  // Add more questions to a session
+  const addMoreQuestions = async () => {};
+
+  useEffect(() => {
+    if (sessionId) {
+      fetchSessionDataById();
+    }
+    return () => {};
+  }, []);
+  return (
+    <DashboardLayout>
+      <RoleInfoHeader
+        role={sessionData?.role || ""}
+        topicsToFocus={sessionData?.topicsToFocus || ""}
+        experience={sessionData?.experience || "-"}
+        questions={sessionData?.questions?.length || "-"}
+        description={sessionData?.description || ""}
+        lastUpdatedAt={
+          sessionData?.updatedAt
+            ? moment(sessionData.updatedAt).format("Do MMM YYYY")
+            : ""
+        }
+      />
+      <div className="container mx-auto pt-4 pb-4 px-4 md:px-0">
+        <h2 className="text-lg font-semibold text-black">Interview Q&A</h2>
+        <div className="grid grid-cols-12 gap-4 mt-5 mb-10">
+          <div
+            className={`col-span-12 ${
+              openLearnMoreDrawer ? "md:col-span-7" : "md:col-span-8"
+            }`}
+          >
+            <AnimatePresence>
+              {sessionData?.questions?.map((data, index) => {
+                return (
+                  <motion.div
+                    key={data._id || index}
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, scale: 0.95 }}
+                    transition={{
+                      duration: 0.4,
+                      type: "spring",
+                      stiffness: 100,
+                      delay: index * 0.1,
+                      damping: 15,
+                    }}
+                    layout
+                    layoutId={`question-${data._id || index}`}
+                  >
+                    <QuestionCard
+                      question={data?.question}
+                      answer={data?.answer}
+                      onLearnMore={() =>
+                        generateConceptExplanation(data.question)
+                      }
+                      isPinned={data?.isPinned}
+                      onTogglePin={() => togglePinQuestion(data._id)}
+                    />
+                  </motion.div>
+                );
+              })}
+            </AnimatePresence>
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Interview;
